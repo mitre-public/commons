51ed67643aaf8c2eab83dba0db8dc0a008d8cd3b
Parker Jon I <jiparker@mitre.org>
2023-03-14 12:12:58 -0400

    Improved TranslatingConsumer and FilteredConsumer | Removed TwoWayConsumer (its OBE due to FilteredConsumer)

--- src/main/java/org/mitre/caasd/commons/FilteredConsumer.java
+++ src/main/java/org/mitre/caasd/commons/FilteredConsumer.java
@@ -16,17 +16,21 @@
 
 package org.mitre.caasd.commons;
 
+import static java.util.Objects.requireNonNull;
+
 import java.io.Serializable;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
 
 /**
- * A FilteredConsumer decorates any Consumer with a filtering stage provided by a Predicate.
+ * A FilteredConsumer decorates a Consumer with a preemptive filtering Predicate. The inner Consumer
+ * only receives input items that pass the filter's boolean test.
+ * <p>
+ * If desired, inputs that fail the filter's boolean test will be passed to a 2nd Consumer. In this
+ * mode, a FilteredConsumer can be thought of as a "T-junction" that routes data according to the
+ * output of the filter's boolean test.
  *
- * <p>In other words, the wrapped Consumer only receives input items that have already by "passed"
- * the filter. Inputs that do no pass the filter are ignored.
- *
- * @param <T> The type of the wrapped Consumer
+ * @param <T> The type of data used within these functions
  */
 public class FilteredConsumer<T> implements Consumer<T>, Serializable {
 
@@ -34,24 +38,71 @@ public class FilteredConsumer<T> implements Consumer<T>, Serializable {
 
     private final Predicate<T> filter;
 
-    private final Consumer<T> uponVerification;
+    private final Consumer<T> whenTrue;
 
-    public FilteredConsumer(Predicate<T> filter, Consumer<T> uponVerification) {
-        this.uponVerification = uponVerification;
-        this.filter = filter;
+    private final Consumer<T> whenFalse;
+
+    /**
+     * Bundle a preemptive filter and a downstream consumer that only receives "passing" items.
+     *
+     * @param filter   A boolean test applied to each incoming item T
+     * @param whenTrue The consumer that receives items that pass the filtering test
+     */
+    public FilteredConsumer(Predicate<T> filter, Consumer<T> whenTrue) {
+        this(
+            filter,
+            whenTrue,
+            inputItem -> {}  //discard items that fail the filter, basically /dev/null.
+        );
     }
 
     /**
-     * Accept and input item, pass it to the wrapped consumer if the input passes the filtering
-     * step.
+     * Bundle a preemptive filter and two downstream consumers.  One consumer receives only the
+     * "passing" items, the other consumer receives only the "failing" items.
      *
-     * @param item An input that passed to the wrapped consumer if and only if the input first
-     *             passed the Predicate filter provided at construction.
+     * @param filter    A boolean test applied to each incoming item T
+     * @param whenTrue  The consumer that receives items that pass the filtering test
+     * @param whenFalse The consumer that receives items that fail the filtering test
+     */
+    public FilteredConsumer(Predicate<T> filter, Consumer<T> whenTrue, Consumer<T> whenFalse) {
+        requireNonNull(filter);
+        requireNonNull(whenTrue);
+        requireNonNull(whenFalse);
+        this.filter = filter;
+        this.whenTrue = whenTrue;
+        this.whenFalse = whenFalse;
+    }
+
+    /**
+     * Accept an input item, apply the boolean test, and pass it to the corresponding consumer.
+     *
+     * @param item An input that will be tested and routed.
      */
     @Override
     public void accept(T item) {
+
         if (filter.test(item)) {
-            uponVerification.accept(item);
+            whenTrue.accept(item);
+        } else {
+            whenFalse.accept(item);
         }
+    }
+
+    /** @return The filtering Predicate provided at construction time. */
+    public Predicate<T> filter() {
+        return this.filter;
+    }
+
+    /** @return The Consumer provided at construction for receiving "passing" inputs. */
+    public Consumer<T> whenTrue() {
+        return this.whenTrue;
+    }
+
+    /**
+     * @return The Consumer provided at construction for receiving "failing" inputs.  This will
+     *     be a "NO OP Lambda" (e.g. `x -> {}`) when a 2nd Consumer is not supplied
+     */
+    public Consumer<T> whenFalse() {
+        return this.whenFalse;
     }
 }
