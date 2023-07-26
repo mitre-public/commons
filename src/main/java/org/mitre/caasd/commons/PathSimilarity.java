cee68dced9025701ff73f2c7b253da0658ce932a
Jon Parker <jiparker@mitre.org>
2023-07-26 10:56:09 -0400

    feat: Adding PathSimilarity

---
+++ src/main/java/org/mitre/caasd/commons/PathSimilarity.java
@@ -0,0 +1,131 @@
+package org.mitre.caasd.commons;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static java.lang.Math.*;
+import static java.util.Objects.requireNonNull;
+import static org.mitre.caasd.commons.Spherical.angleDifference;
+
+/**
+ * This class has methods that can be the "foundation" of {@code DistanceMetric}
+ * implementations that help sort and search regularly sized LatLongPaths.
+ * <p>
+ * The "general idea" is to use a LocalPolyInterpolator to smooth and sample object position data at
+ * regular time intervals. This process converts a datafeed with irregular timing to a datafeed that
+ * has a "fixed frequency" and can be chopped into snippets and searched.
+ */
+public class PathSimilarity {
+
+    /**
+     * Compute an accurate, but computationally taxing, "similarity score" between these two Paths.
+     * <p>
+     * The similarity computed here is the NEGATIVE sum of the distance between each "LatLong pair"
+     * taken from the paths (e.g. the distance btw the 1st LatLong from both paths PLUS the distance
+     * btw the 2nd LatLong from both paths PLUS the distance btw the 3rd LatLong from both paths
+     * ...).
+     * <p>
+     * The similarity between two identical paths will be 0.  The similarity between two nearly
+     * identical paths will be a small negative number.  The similarity between two very different
+     * paths will be a large negative number.
+     * <p>
+     * The computation requires both Paths to have the same size.  This is an important requirement
+     * for making a DistanceMetric using this method.
+     *
+     * @param p1 A path
+     * @param p2 Another path
+     *
+     * @return A similarity score (a negative number unless the paths are identical). Maximizing a
+     *     similarity score finds the Most similar paths.
+     */
+    public static double slowSimilarity(LatLongPath p1, LatLongPath p2) {
+        //ACCURATE BUT SLOW
+
+        requireNonNull(p1);
+        requireNonNull(p2);
+        checkArgument(p1.size() == p2.size(), "Paths must have same size");
+        double distanceSum = 0;
+        int n = p1.size();
+        for (int i = 0; i < n; i += 1) {
+            LatLong mine = p1.get(i);
+            LatLong his = p2.get(i);
+            distanceSum += mine.distanceInNM(his);
+        }
+
+        //return the
+        return 0 - distanceSum;
+    }
+
+    /**
+     * Compute an approximate, and easily computed, "similarity score" between these two Paths.
+     * <p>
+     * The similarity computed here is optimized for fast computation.  It is maximally accurate
+     * when paths are similar. This similarity score does NOT use expensive floating point
+     * trigonometric operations because we believe accuracy is highest we care the most (when paths
+     * are similar) and accuracy is lowest when we care the least (when paths are obviously
+     * dissimilar).
+     * <p>
+     * The similarity score computed here is the NEGATIVE ("integral of latitude differences" plus
+     * the "integral of the longitude differences").
+     * <p>
+     * The similarity between two identical paths will be 0.  The similarity between two nearly
+     * identical paths will be a small negative number.  The similarity between two very different
+     * paths will be a large negative number.
+     * <p>
+     * The computation requires both Paths to have the same size.  This is an important requirement
+     * for making a DistanceMetric using this method.
+     *
+     * @param p1 A path
+     * @param p2 Another path
+     *
+     * @return A similarity score (a negative number unless the paths are identical). Maximizing a
+     *     similarity score finds the Most similar paths.
+     */
+    public static double similarity(LatLongPath p1, LatLongPath p2) {
+        //USUALLY GOOD ENOUGH FOR PICKING THE "MOST SIMILAR PATH" BECAUSE ACCURACY IMPROVES AS PATHS GET MORE AND MORE SIMILAR
+
+        requireNonNull(p1);
+        requireNonNull(p2);
+        checkArgument(p1.size() == p2.size());
+
+        double sumOfLatitudeDeltas = latitudeDist(p1.latitudes(), p2.latitudes());
+        double sumOfLongitudeDeltas = longitudeDist(p1.longitudes(), p2.longitudes());
+
+        return -sumOfLatitudeDeltas - sumOfLongitudeDeltas;
+    }
+
+    /** Sum the "deltas" for each pair of latitude values. */
+    static double latitudeDist(double[] lats1, double[] lats2) {
+
+        double totalDelta = 0;
+
+        for (int i = 0; i < lats1.length; i++) {
+            totalDelta += latitudeDelta(lats1[i], lats2[i]);
+        }
+
+        return totalDelta;
+    }
+
+    /** Sum the "deltas" for each pair of longitude values. */
+    static double longitudeDist(double[] longs1, double[] longs2) {
+
+        double totalDelta = 0;
+        int n = longs1.length;
+
+        for (int i = 0; i < n; i++) {
+            totalDelta += longitudeDelta(longs1[i], longs2[i]);
+        }
+
+        return totalDelta;
+    }
+
+    /** Computes the "delta in degrees" between two valid latitudes (always positive). */
+    static double latitudeDelta(double latitude1, double latitude2) {
+        return abs(latitude1 - latitude2);
+    }
+
+
+    /** Computes the "delta in degrees" between two valid longitudes (always positive). */
+    static double longitudeDelta(double longitude1, double longitude2) {
+        //be careful with longitude -- the international date line is a problem
+        return abs(angleDifference(longitude1 - longitude2));
+    }
+}
---
+++ src/test/java/org/mitre/caasd/commons/PathSimilarityTest.java
@@ -0,0 +1,155 @@
+package org.mitre.caasd.commons;
+
+import static java.lang.Math.abs;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mitre.caasd.commons.PathSimilarity.*;
+
+import org.junit.jupiter.api.Test;
+
+class PathSimilarityTest {
+
+    @Test
+    public void testLongitudeDist() {
+
+        assertThat(longitudeDelta(0.0, 1.0), is(1.0));
+        assertThat(longitudeDelta(1.0, 0.0), is(1.0));
+
+        assertThat(longitudeDelta(179.5, -179.5), is(1.0));
+        assertThat(longitudeDelta(-179.5, 179.5), is(1.0));
+
+        assertThat(longitudeDelta(0, -179.5), is(179.5));
+        assertThat(longitudeDelta(-179.5, 0), is(179.5));
+
+        assertThat(longitudeDelta(0, 22.5), is(22.5));
+        assertThat(longitudeDelta(22.5, 0), is(22.5));
+    }
+
+    @Test
+    public void testLatitudeDist() {
+
+        assertThat(latitudeDelta(0.0, 1.0), is(1.0));
+        assertThat(latitudeDelta(1.0, 0.0), is(1.0));
+
+        assertThat(latitudeDelta(89.5, -89.5), is(179.0));
+        assertThat(latitudeDelta(-89.5, 89.5), is(179.0));
+
+        assertThat(latitudeDelta(0, -89.5), is(89.5));
+        assertThat(latitudeDelta(-89.5, 0), is(89.5));
+
+        assertThat(latitudeDelta(0, 22.5), is(22.5));
+        assertThat(latitudeDelta(22.5, 0), is(22.5));
+    }
+
+    @Test
+    public void testAccurateSimilarity() {
+
+        LatLong a = LatLong.of(032.82810, -097.36291);
+        LatLong b = LatLong.of(032.82960, -097.36299);
+        LatLong c = LatLong.of(032.83143, -097.36338);
+        LatLong d = LatLong.of(032.83332, -097.36362);
+
+        LatLongPath path1 = new LatLongPath(a, b, c, d);
+
+        LatLong e = LatLong.of(032.83482, -097.36409);
+        LatLong f = LatLong.of(032.83645, -097.36448);
+        LatLong g = LatLong.of(032.83801, -097.36487);
+        LatLong h = LatLong.of(032.83951, -097.36511);
+
+        LatLongPath path2 = new LatLongPath(e, f, g, h);
+
+        assertThat(slowSimilarity(path1, path1), is(0.0));
+        assertThat(slowSimilarity(path2, path2), is(0.0));
+
+        double expectedDist = a.distanceInNM(e) + b.distanceInNM(f) + c.distanceInNM(g) + d.distanceInNM(h);
+
+        assertThat(slowSimilarity(path1, path2), is(-expectedDist));
+        assertThat(slowSimilarity(path1, path2), is(-expectedDist));
+        assertThat(slowSimilarity(path1, path2), is(slowSimilarity(path2, path1)));
+    }
+
+    @Test
+    public void testLatDist() {
+
+        LatLong a = LatLong.of(032.82810, -097.36291);
+        LatLong b = LatLong.of(032.82960, -097.36299);
+        LatLong c = LatLong.of(032.83143, -097.36338);
+        LatLong d = LatLong.of(032.83332, -097.36362);
+
+        LatLongPath path1 = new LatLongPath(a, b, c, d);
+
+        LatLong e = LatLong.of(032.83482, -097.36409);
+        LatLong f = LatLong.of(032.83645, -097.36448);
+        LatLong g = LatLong.of(032.83801, -097.36487);
+        LatLong h = LatLong.of(032.83951, -097.36511);
+
+        LatLongPath path2 = new LatLongPath(e, f, g, h);
+
+        double actualLatDist = latitudeDist(path1.latitudes(), path2.latitudes());
+
+        double expectLatDist = abs(a.latitude() - e.latitude())
+            + abs(b.latitude() - f.latitude())
+            + abs(c.latitude() - g.latitude())
+            + abs(d.latitude() - h.latitude());
+
+        assertEquals(actualLatDist, expectLatDist, 0.000000001);
+    }
+
+    @Test
+    public void testLongDist() {
+
+        LatLong a = LatLong.of(032.82810, -097.36291);
+        LatLong b = LatLong.of(032.82960, -097.36299);
+        LatLong c = LatLong.of(032.83143, -097.36338);
+        LatLong d = LatLong.of(032.83332, -097.36362);
+
+        LatLongPath path1 = new LatLongPath(a, b, c, d);
+
+        LatLong e = LatLong.of(032.83482, -097.36409);
+        LatLong f = LatLong.of(032.83645, -097.36448);
+        LatLong g = LatLong.of(032.83801, -097.36487);
+        LatLong h = LatLong.of(032.83951, -097.36511);
+
+        LatLongPath path2 = new LatLongPath(e, f, g, h);
+
+        double actualLongDist = longitudeDist(path1.longitudes(), path2.longitudes());
+
+        double expectLongDist = abs(a.longitude() - e.longitude())
+            + abs(b.longitude() - f.longitude())
+            + abs(c.longitude() - g.longitude())
+            + abs(d.longitude() - h.longitude());
+
+        assertEquals(actualLongDist, expectLongDist, 0.000000001);
+    }
+
+    @Test
+    public void testSimpleSimilarity() {
+
+        LatLong a = LatLong.of(032.82810, -097.36291);
+        LatLong b = LatLong.of(032.82960, -097.36299);
+        LatLong c = LatLong.of(032.83143, -097.36338);
+        LatLong d = LatLong.of(032.83332, -097.36362);
+
+        LatLongPath path1 = new LatLongPath(a, b, c, d);
+
+        LatLong e = LatLong.of(032.83482, -097.36409);
+        LatLong f = LatLong.of(032.83645, -097.36448);
+        LatLong g = LatLong.of(032.83801, -097.36487);
+        LatLong h = LatLong.of(032.83951, -097.36511);
+
+        LatLongPath path2 = new LatLongPath(e, f, g, h);
+
+        //self similarity is always zero
+        assertEquals(similarity(path1, path1), 0.0, 0.00000001);
+        assertEquals(similarity(path2, path2), 0.0, 0.00000001);
+
+        double expectedSim = -latitudeDist(path1.latitudes(), path2.latitudes())
+            - longitudeDist(path1.longitudes(), path2.longitudes());
+
+        assertThat(similarity(path1, path2), is(expectedSim));
+        assertThat(similarity(path2, path1), is(expectedSim));
+        assertThat(similarity(path1, path2), is(similarity(path2, path1)));
+
+    }
+}
