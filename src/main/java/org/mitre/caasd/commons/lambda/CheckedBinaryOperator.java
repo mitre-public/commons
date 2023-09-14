ff8a52192b824c1c7fe187a27e1e99fade4834d4
Adric Eckstein <aeckstein@mitre.org>
2023-09-14 10:14:43 -0400

    add checked lambda funcs that can be easily demoted to standard lambdas

---
+++ src/main/java/org/mitre/caasd/commons/lambda/CheckedBinaryOperator.java
@@ -0,0 +1,43 @@
+/*
+ *    Copyright 2023 The MITRE Corporation
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package org.mitre.caasd.commons.lambda;
+
+import java.util.function.BinaryOperator;
+
+import org.mitre.caasd.commons.util.DemotedException;
+
+/**
+ * Extension of the {@link BinaryOperator} interface for a checked lambda
+ * function
+ */
+@FunctionalInterface
+public interface CheckedBinaryOperator<T> extends CheckedBiFunction<T, T, T> {
+
+    /**
+     * Demote the {@link FunctionalInterface} that throws an {@link Exception} to a
+     * {@link BinaryOperator}
+     */
+    static <T> BinaryOperator<T> demote(CheckedBinaryOperator<T> func) {
+        return (t, u) -> {
+            try {
+                return func.apply(t, u);
+            } catch (Exception e) {
+                throw DemotedException.demote(e);
+            }
+        };
+    }
+}
---
+++ src/main/java/org/mitre/caasd/commons/lambda/CheckedFunction.java
@@ -0,0 +1,45 @@
+/*
+ *    Copyright 2023 The MITRE Corporation
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package org.mitre.caasd.commons.lambda;
+
+import java.util.function.Function;
+
+import org.mitre.caasd.commons.util.DemotedException;
+
+/**
+ * Extension of the {@link Function} interface for a checked lambda
+ * function
+ */
+@FunctionalInterface
+public interface CheckedFunction<S, T> {
+
+    T apply(S t) throws Exception;
+
+    /**
+     * Demote the {@link FunctionalInterface} that throws an {@link Exception} to a
+     * {@link Function}
+     */
+    static <S, T> Function<S, T> demote(CheckedFunction<S, T> func) {
+        return x -> {
+            try {
+                return func.apply(x);
+            } catch (Exception e) {
+                throw DemotedException.demote(e);
+            }
+        };
+    }
+}
