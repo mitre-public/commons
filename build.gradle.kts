5b15394f0c64bf22592aae03a050df098e717bb9
Jon Parker <jiparker@mitre.org>
2023-10-12 15:04:57 -0400

    Update gradle for maven central publication

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,9 @@ plugins {
     `maven-publish`
 }
 
+group = "org.mitre"
+version = "0.0.54-SNAPSHOT"
+
 repositories {
     mavenCentral()
     mavenLocal()
@@ -38,12 +41,43 @@ java {
 
 publishing {
     publications {
-        create<MavenPublication>("maven") {
-            //groupId does not need to be set here
-            artifactId = project.name
-            //version does not need to be set here
-
+        create<MavenPublication>("mavenJava") {
+            artifactId = "commons"
             from(components["java"])
+            versionMapping {
+                usage("java-api") {
+                    fromResolutionOf("runtimeClasspath")
+                }
+                usage("java-runtime") {
+                    fromResolutionResult()
+                }
+            }
+            pom {
+                name.set(project.name)
+                description.set("MITRE Commons Library for Aviation")
+                url.set("https://github.com/mitre-public/commons")
+
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("jparker")
+                        name.set("Jon Parker")
+                        email.set("jiparker@mitre.org")
+                    }
+                }
+
+                //REQUIRED! To publish to maven central (from experience the leading "scm:git" is required too)
+                scm {
+                    connection.set("scm:git:https://github.com/mitre-public/commons.git")
+                    developerConnection.set("scm:git:ssh://git@github.com:mitre-public/commons.git")
+                    url.set("https://github.com/mitre-public/commons")
+                }
+            }
         }
     }
 }
