8f4735010295392022b34e16d9d367d2425ac730
Jon Parker <jiparker@mitre.org>
2025-05-15 10:52:18 -0400

    CI needs to use Java 17
    * Fixed CI
    * Implemented important part of PathPair

--- src/main/java/org/mitre/caasd/commons/PathPair.java
+++ src/main/java/org/mitre/caasd/commons/PathPair.java
@@ -3,11 +3,55 @@ package org.mitre.caasd.commons;
 import static com.google.common.base.Preconditions.checkArgument;
 import static java.util.Objects.requireNonNull;
 
+/**
+ * A PathPair combines two VehiclePaths that have the same size.
+ * <p>
+ * This record is for finding "similar Vehicle events". The idea is: "When important pair-wise
+ * vehicle interactions occur extract the path those two vehicles traveled around the time of the
+ * event. Then, keep a record of that "pair-wise interaction". This allows multiple important
+ * events that have the "similar paths" to be found.
+ *
+ * @param path0
+ * @param path1
+ */
 public record PathPair(VehiclePath path0, VehiclePath path1) {
 
     public PathPair {
         requireNonNull(path0);
         requireNonNull(path1);
         checkArgument(path0.size() == path1.size());
+    }
+
+    /** @return The number of "locations" in each path (which must be the same). */
+    public int size() {
+        return path0.size();
+    }
+
+    /** Compute the distance between these two paths (use the full paths) */
+    public static double distanceBtw(PathPair a, PathPair b) {
+        requireNonNull(a);
+        requireNonNull(b);
+        checkArgument(a.size() == b.size(), "Paths must have same size");
+
+        return distanceBtw(a, b, a.size());
+    }
+
+    /** Compute the distance between these two paths using just the first n points of the paths */
+    public static double distanceBtw(PathPair a, PathPair b, int n) {
+        requireNonNull(a);
+        requireNonNull(b);
+        checkArgument(n >= 0);
+        checkArgument(a.size() >= n, "PathPair1 does not have the required length");
+        checkArgument(b.size() >= n, "PathPair2 does not have the required length");
+
+        // We don't know how to pair off the 4 vehicles in this "Path Pair" comparison
+
+        // a0-to-b0 + a1-to-b1
+        double opt1 = VehiclePath.distanceBtw(a.path0, b.path0, n) + VehiclePath.distanceBtw(a.path1, b.path1);
+
+        // a0-to-b1 + a1-to-b0
+        double opt2 = VehiclePath.distanceBtw(a.path0, b.path1, n) + VehiclePath.distanceBtw(a.path1, b.path0);
+
+        return Math.min(opt1, opt2);
     }
 }
---
+++ src/test/java/org/mitre/caasd/commons/PathPairTest.java
@@ -0,0 +1,63 @@
+package org.mitre.caasd.commons;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.is;
+
+import org.junit.jupiter.api.Test;
+
+class PathPairTest {
+
+    @Test
+    void distanceChoosesCorrectArrangement() {
+
+        // Want 2 wildly different VehiclePaths.
+        // The pack those SAME to VehiclePaths into 2 PathPairs but arranged different (i.e. ab and ba)
+
+        // LatLong paths are DIFFERENT!
+        LatLong64Path lat_path_1 = LatLong64Path.from(LatLong.of(1.0, 2.0), LatLong.of(3.0, 4.0));
+        LatLong64Path lat_path_2 = LatLong64Path.from(LatLong.of(2.0, 1.0), LatLong.of(4.0, 3.0));
+
+        // Alt paths are DIFFERENT!
+        AltitudePath vert_path_1 = AltitudePath.from(Distance.ofFeet(100), Distance.ofFeet(200));
+        AltitudePath vert_path_2 = AltitudePath.from(Distance.ofFeet(20000), Distance.ofFeet(3000));
+
+        // Thus VehiclePaths are DIFFERENT
+        VehiclePath vp_1 = new VehiclePath(lat_path_1, vert_path_1);
+        VehiclePath vp_2 = new VehiclePath(lat_path_2, vert_path_2);
+
+        PathPair a = new PathPair(vp_1, vp_2);
+        PathPair b = new PathPair(vp_2, vp_1);
+
+        // Paths are wildly different ... but the different pairs are good
+        assertThat(VehiclePath.distanceBtw(vp_1, vp_2), greaterThan(1053393.0));
+
+        assertThat(PathPair.distanceBtw(a, b), is(0.0));
+        assertThat(PathPair.distanceBtw(b, a), is(0.0));
+    }
+
+    @Test
+    void distanceReflectsAltitude() {
+
+        // These are the same
+        LatLong64Path lat_path_1 = LatLong64Path.from(LatLong.of(1.0, 2.0), LatLong.of(3.0, 4.0));
+        LatLong64Path lat_path_2 = LatLong64Path.from(LatLong.of(1.0, 2.0), LatLong.of(3.0, 4.0));
+
+        // Alt paths are slightly different
+        AltitudePath vert_path_1 = AltitudePath.from(Distance.ofFeet(100), Distance.ofFeet(200));
+        AltitudePath vert_path_2 = AltitudePath.from(Distance.ofFeet(200), Distance.ofFeet(400));
+
+        // Thus VehiclePaths are DIFFERENT
+        VehiclePath vp_1 = new VehiclePath(lat_path_1, vert_path_1);
+        VehiclePath vp_2 = new VehiclePath(lat_path_2, vert_path_2);
+
+        PathPair a = new PathPair(vp_1, vp_2);
+        PathPair b = new PathPair(vp_2, vp_1);
+
+        // Paths are offset by 300 foot
+        assertThat(VehiclePath.distanceBtw(vp_1, vp_2), is(300.0));
+
+        assertThat(PathPair.distanceBtw(a, b), is(0.0));
+        assertThat(PathPair.distanceBtw(b, a), is(0.0));
+    }
+}
